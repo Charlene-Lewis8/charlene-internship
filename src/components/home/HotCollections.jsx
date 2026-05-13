b37e436a785baeb7a10781e370edf23705b4c5ac
Charlene-Lewis8 <charmarlew08@gmail.com>
2026-05-12 22:11:47 -0400

    add keen-slider

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
+import 'keen-slider/keen-slider.min.css';
+import {useKeenSlider} from 'keen-slider/react';
 
 const HotCollections = () => {
+  const [sliderRef, instanceRef] = useKeenSlider({
+    loop: true,
+    mode: "free",
+    slides: {
+      perView: 3,
+    },
+    slideChanged() {
+      console.log('slide changed')
+    },
+  }) 
+
   const [collections, setCollections] = useState([]);
   
   useEffect(() => {
@@ -24,22 +35,23 @@ const HotCollections = () => {
     <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
-          <div className="col-lg-12">
+          <div className="col-lg-12" style={{ position: 'relative'}}>
             <div className="text-center">
               <h2>Hot Collections</h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+          <div ref={sliderRef} className="keen-slider">
           {collections.map((item, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12 keen-slider__slide" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
-                <Link to= "/item-details ">
+                <Link to={`/item-details/${item.nftId}`}>
                     <img src={item.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
-                <Link to="/author">
+                <Link to={`/author/${item.authorId}`}>
                     <img className="lazy pp-coll" src={item.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
@@ -48,11 +60,12 @@ const HotCollections = () => {
                   <Link to="/explore">
                     <h4>{item.title}</h4>
                   </Link>
-                  <span>{item.code}</span>
+                  <span>ERC-{item.code}</span>
                 </div>
               </div>
             </div>
           ))}
+          </div>
         </div>
       </div>
     </section>
